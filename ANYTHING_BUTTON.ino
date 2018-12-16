60112d0a16e9a8d03a5b64a4174130680a0a973b
Scott Linenberger <scott.linenberger@gmail.com>
2018-12-16 11:40:36 -0800

    add pin setup

--- ANYTHING_BUTTON.ino
+++ ANYTHING_BUTTON.ino
@@ -1,9 +1,36 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Arduino.h>
 
+/* pin declarations */
+#define PIN_LED_GREEN 12
+#define PIN_LED_BLUE 13
+#define PIN_LED_RED 14
+#define PIN_BUTTON 4
+
+void setup() {
+  /* begin serial for debugging */
+  Serial.begin(115200);
+
+  /* get pins ready for use */
+  initPins();
+}
+
+void initPins() {
+  /* init RGB LED pins */
+  pinMode(PIN_LED_RED, OUTPUT);
+  pinMode(PIN_LED_GREEN, OUTPUT);
+  pinMode(PIN_LED_BLUE, OUTPUT);
+
+  /* init button pin */
+  pinMode(PIN_BUTTON, INPUT);
+}
+
+void ledStatusOff() {
+  /* turn R, G and B off */
+  digitalWrite(PIN_LED_RED, HIGH);
+  digitalWrite(PIN_LED_GREEN, HIGH);
+  digitalWrite(PIN_LED_BLUE, HIGH);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
 }
