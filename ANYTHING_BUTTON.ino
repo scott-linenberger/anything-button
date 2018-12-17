a1cd32f38dbd128b8286eec1b74f9c17ee415c15
Scott Linenberger <scott.linenberger@gmail.com>
2018-12-17 07:46:49 -0800

    wire button and status led into sketch

--- ANYTHING_BUTTON.ino
+++ ANYTHING_BUTTON.ino
@@ -1,4 +1,6 @@
 #include <Arduino.h>
+#include "StatusLed.h"
+#include "Button.h"
 
 /* pin declarations */
 #define PIN_LED_GREEN 12
@@ -6,12 +8,40 @@
 #define PIN_LED_RED 14
 #define PIN_BUTTON 4
 
+/* = BUTTON SETUP = */
+/* time to wait between presses in ms */
+uint16_t TIMEOUT_DEBOUNCE = 1000;
+
+/* how long the button must be presse in ms */
+uint16_t TIMEOUT_PRESS = 100;
+boolean STATE_BUTTON_TRIGGERED = HIGH;
+
+Button button = Button(
+  PIN_BUTTON,
+  TIMEOUT_DEBOUNCE,
+  TIMEOUT_PRESS,
+  STATE_BUTTON_TRIGGERED);
+
+/* = STATUS LED SETUP = */
+boolean ledOnState = LOW;
+
+StatusLed statusLed = StatusLed(
+  PIN_LED_RED,
+  PIN_LED_GREEN,
+  PIN_LED_BLUE,
+  ledOnState
+);
+
 void setup() {
   /* begin serial for debugging */
   Serial.begin(115200);
 
-  /* get pins ready for use */
+  /* get pins ready */
   initPins();
+
+  /* init objects */
+  statusLed.begin();
+  button.begin();
 }
 
 void initPins() {
@@ -24,13 +54,15 @@ void initPins() {
   pinMode(PIN_BUTTON, INPUT);
 }
 
-void ledStatusOff() {
-  /* turn R, G and B off */
-  digitalWrite(PIN_LED_RED, HIGH);
-  digitalWrite(PIN_LED_GREEN, HIGH);
-  digitalWrite(PIN_LED_BLUE, HIGH);
-}
-
 void loop() {
+  if (button.isPressed()) {
+    Serial.println("Pressed!");
+    
+    statusLed.red();
+    delay(500);
+    statusLed.off();
+  }
 
+  /* call the status LED's run method on each loop */
+  statusLed.run();
 }
